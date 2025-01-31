a345f036535795c10e3348527d9a15b7f5e20704
camille-norrito <camille.norrito@edu.univ-fcomte.fr>
2025-01-31 14:46:17 +0100

    imgUrl

---
+++ src/backend.mjs
@@ -0,0 +1,53 @@
+//Camille Norrito
+
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function allMaisons() {
+    let records = await pb.collection('Maisons').getFullList();
+    records = records.map((maison) => {
+        maison.imgUrl = pb.files.getURL(maison, maison.images[0]); 
+        console.log ('maison on veut afficher url',maison.imgUrl)
+        return maison;
+    });
+    return records;
+}
+
+
+export async function oneID(id) {
+    const oneRecord = await pb.collection('Maisons').getOne(id);
+    return oneRecord;
+}
+
+export async function allMaisonsFavori() {
+    const records = await pb.collection('Maisons').getFullList({
+        filter: 'favori = true'
+    });
+    return records;
+}
+
+export async function allMaisonsSorted() {
+    const records = await pb.collection('Maisons').getFullList({
+        sort: 'prix'
+    });
+    return records;
+}
+
+export async function bySurface(surface) {
+    const records = await pb.collection('Maisons').getFullList({
+        filter: `surface > ${surface}`
+    });
+    return records;
+}
+
+export async function surfaceORprice(surface, price) {
+    const records = await pb.collection('Maisons').getFullList({
+        filter: `surface > ${surface} || prix < ${price}`
+    });
+    return records;
+}
+
+export async function getAgentByID(id) {
+    const agent = await pb.collection('agent').getOne(id);
+    return agent;
+}
